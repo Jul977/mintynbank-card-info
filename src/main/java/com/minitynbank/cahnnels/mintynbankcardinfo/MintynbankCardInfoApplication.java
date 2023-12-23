795d12a0815076c7f384f804d0b0ab32ed3b578b
Emmanuel-Irabor <emmanuelirabor@palmpay-inc.com>
2023-12-23 15:24:25 +0100

    Initial commit

---
+++ src/main/java/com/minitynbank/cahnnels/mintynbankcardinfo/MintynbankCardInfoApplication.java
@@ -0,0 +1,13 @@
+package com.minitynbank.cahnnels.mintynbankcardinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MintynbankCardInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MintynbankCardInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minitynbank/cahnnels/mintynbankcardinfo/MintynbankCardInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minitynbank.cahnnels.mintynbankcardinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MintynbankCardInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
